6cc59a76b0a81810337e3149e10dd003aef4ca8c
OlgierdNieszporek <olgierdnieszporek@gmail.com>
2022-11-02 12:16:05 +0100

    fully functional upload and image inverse

--- invertPhoto.py
+++ invertPhoto.py
@@ -1,6 +1,10 @@
 import os
+import re
 import shutil
+
+import PIL
 from fastapi import File
+from PIL import Image, ImageOps
 
 
 def save_image(file: File(), filename):
@@ -12,3 +16,35 @@ def save_image(file: File(), filename):
 def move_image(filename):
     shutil.move(os.path.join(filename), os.path.join('images', filename))
     return 0
+
+
+def invert_photo_colors(filename):
+    no_suffix_filename = remove_suffix(filename)
+    print(no_suffix_filename)
+    image = Image.open('images/' + filename)
+    if image.mode == 'RGBA':
+        r, g, b, a = image.split()
+        rgb_image = Image.merge('RGB', (r, g, b))
+
+        inverted_image = PIL.ImageOps.invert(rgb_image)
+
+        r2, g2, b2 = inverted_image.split()
+
+        final_transparent_image = Image.merge('RGBA', (r2, g2, b2, a))
+
+        final_transparent_image.save('images/images_inverted/' + no_suffix_filename + '.png')
+
+    else:
+        inverted_image = PIL.ImageOps.invert(image)
+        inverted_image.save('images/images_inverted/' + no_suffix_filename + '.png')
+    return 0
+
+def remove_suffix(filename):
+    filename = re.sub('\.JPG$', '', filename)
+    return filename
+
+
+def display_image(filename):
+    image_to_print = remove_suffix(filename) + ".png"
+    with open('images/images_inverted/' + image_to_print, mode="rb") as file_like:
+        yield from file_like
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import time
+from fastapi import FastAPI, Path, UploadFile
 
-from PIL import Image
-from fastapi import FastAPI, Path, HTTPException, UploadFile, responses
-from fastapi.openapi.models import Response
 from starlette.requests import Request
-from starlette.responses import FileResponse, HTMLResponse
+from starlette.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
-from deta import Drive, Deta
-import os
 
+from LimitUploadSize import LimitUploadSize
 from primeNumbers import *
 from invertPhoto import *
 
 app = FastAPI()
+
+app.add_middleware(LimitUploadSize, max_upload_size=12_000_000)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -33,30 +31,14 @@ async def read_item(
 
 @app.post("/upload-photo")
 async def read_item(file: UploadFile = File(..., format=".jpeg")):
-    if len(await file.read()) >= 12000000:
-        raise HTTPException(status_code=413, detail="Image is greater than 12MB")
-    #if file.content_type != "image/jpeg":
-       # raise HTTPException(status_code=400, detail="File content type must be jpg")
     save_image(file, file.filename)
     move_image(file.filename)
     return {"name_of_file": file.filename}
 
 
-@app.get("/picture/invert/{filename}", response_class=HTMLResponse)
-async def read_item(request: Request, filename: str = Path(
+@app.get("/picture/invert/{filename}")
+async def display_photo(request: Request, filename: str = Path(
     description="The name of the file"
 )):
-    out = []
-    for filename in os.listdir("images"):
-        out.append({
-            "name": filename.split(".")[0],
-            "path": "/images" + filename
-        }
-        )
-        return out
-
-    # wait_time = 0.5
-    # time.sleep(wait_time)
-    # print('images/' + filename)
-    # img = Image.open('images/'+filename)
-    # return Response(content=img, media_type="image/jpeg")
+    invert_photo_colors(filename)
+    return StreamingResponse(display_image(filename), media_type="image/png")
